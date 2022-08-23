b1c0e055aaa81810bd1e31c68473bbb744f70d3d
Rin <nian.ci@hotmail.com>
2022-08-24 04:20:05 +0800

    Showing contents of chest on monitor

--- computers/basic/learn_inventory.lua
+++ computers/basic/learn_inventory.lua
@@ -1,14 +1,18 @@
--- NOT TESTED IN GAME YET
-
--- Display items in chest in the computer and monitor.
+-- Display items in chest in the computer and monitor. (Tested on 4x4 monitors)
 
 -- Main
 chest = peripheral.find("minecraft:chest")
 monitor = peripheral.find("monitor")
-
+ 
+monitor.setTextScale(1)
+ 
 while true do
+    monitor.clear()
+ 
     for slot, item in pairs(chest.list()) do
         print(("%d x %s in slot %d"):format(item.count, item.name, slot))
+        monitor.setCursorPos(1,slot)
         monitor.write(("%d x %s in slot %d"):format(item.count, item.name, slot))
     end
+    sleep(5)
 end
