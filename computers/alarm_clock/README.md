4f8d53f9586dc83317cd1a470c0fab72d6cfc1b1
Rin <nian.ci@hotmail.com>
2022-10-23 14:32:11 +0800

    Alarm clock

---
+++ computers/alarm_clock/README.md
@@ -0,0 +1,12 @@
+# Alarm Clock (SPEAKERS NOT IMPLEMENTED)
+Create a working alarm clock in game. The alarm rings every day at the same time using 24 hours clock.
+
+### File format for creating alarm clock
+Create a file called alarm.txt using ```edit alarm.txt```
+<br>
+In the file
+```
+(name of alarm)=(time)|
+Stop playing=14:25|
+Test=13:20|
+```
---
+++ computers/alarm_clock/alarm_clock.lua
@@ -0,0 +1,67 @@
+-- Alarm clock
+-- Pastebin: https://pastebin.com/QUYTwstC (Updated on 23/10/2022)
+
+local timezoneList = {}
+
+function getTime(timezone)
+    local url = "https://timeapi.io/api/Time/current/zone?timeZone=" ..timezone
+
+    if http.get(url) ~= nil then
+        request = http.get(url).readAll()
+        return textutils.unserialiseJSON(request)
+    else
+        print("This url cannot be found.")
+        return nil
+    end
+end
+
+function getAlarm()
+    local alarmFile = fs.open("alarm.txt", "r")
+    local alarms = alarmFile.readAll()
+    alarmFile.close()
+
+    local alarmArray = {}
+    for str in string.gmatch(alarms, "[^|]+") do
+        str = string.gsub(str, "\n", "")
+        for k, v in string.gmatch(str, "(.+)=(.+)") do
+            alarmArray[k] = v
+        end
+    end
+
+    return alarmArray
+end
+
+-- Main
+print("What is your timezone? (eg. Asia/Singapore)")
+local timezone = io.read()
+local monitor = peripheral.wrap("right")
+
+while true do
+    local timeJSON = getTime(timezone)
+
+    monitor.clear()
+    
+    if timeJSON ~= nil then
+        monitor.setCursorPos(1, 1)
+        monitor.write(timeJSON.time)
+
+        local alarms = getAlarm()
+
+        local cursorHeight = 2
+        for k, v in pairs(alarms) do
+            monitor.setCursorPos(1, cursorHeight)
+
+            if (v == timeJSON.time) then
+                monitor.write(k.." at "..v.. " is ringing")
+            else
+                monitor.write(k.." at "..v)
+            end
+
+            cursorHeight = cursorHeight + 1
+        end
+    else
+        monitor.write("Something went wrong")
+    end
+
+    sleep(1)
+end
